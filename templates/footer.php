e37ec2e259975d90a24492d50becdbd16403fd16
Tiberiu Dascal <tiberiu.purcel@gmail.com>
2023-11-19 10:21:30 +0200

    Fixing URL in footer template

--- templates/footer.php
+++ templates/footer.php
@@ -1,8 +1,19 @@
+<?php
+$uri = $_SERVER['REQUEST_URI'];
+$uri = explode('/', $uri);
+
+$base_url = "http://" . $_SERVER['HTTP_HOST'];
+foreach($uri as $element){
+    if($element != end($uri))
+    $base_url .= $element . "/";
+}
+
+?>
 <footer class="bg-primary text-center text-lg-start">
     <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
         © 2023 Copyright:
-        <a class="text-dark" href="https://localhost/">Bookmarks</a>
+        <a class="text-dark" href="<?=$base_url?>">Bookmarks</a>
     </div>
     <!-- Copyright -->
 </footer>
