3e16fdc5bb76302d6329eecb11786b86e3aaa4c6
Tiberiu Dascal <tiberiu.purcel@gmail.com>
2023-11-19 12:35:34 +0200

    Fixed footer and index

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 session_start();
 
 include 'templates/header.php';
-include 'templates/footer.php';
 
 require_once 'core/utils.php';
 require_once 'classes/Database.php';
 require_once 'classes/User.php';
+
+include 'templates/footer.php';
