925b9d9f5ab8492205ded871023eb3c55b2556e0
Camelia Rasiga <rasigacamelia@gmail.com>
2023-11-21 11:29:10 +0200

    lil changes on footer/ logo design

--- templates/footer.php
+++ templates/footer.php
@@ -9,11 +9,11 @@ foreach($uri as $element){
 }
 
 ?>
-<footer class="bg-primary text-center text-lg-start">
+<footer class="bg-primary text-center text-lg-start text-light">
     <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-        © 2023 Copyright:
-        <a class="text-dark" href="<?=$base_url?>"><i class="fa-solid fa-book-bookmark"></i> Bookmarks&trade;Limited</a>
+        © 2023 Copyright
+        <a class="text-light mx-2" id="logo" href="<?=$base_url?>"><i class="fa-solid fa-book-bookmark"></i> Bookmarks&trade;Limited</a>
     </div>
     <!-- Copyright -->
 </footer>
