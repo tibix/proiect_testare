ea492d7a4c230fe05686e7d6d6152ec538ae7a13
Camelia Rasiga <rasigacamelia@gmail.com>
2023-11-26 21:42:22 +0200

    not done yet...

--- templates/footer.php
+++ templates/footer.php
@@ -10,11 +10,12 @@ foreach($uri as $element){
 
 ?>
 </main>
-<footer class="bg-primary text-center text-lg-start text-light">
+<footer class="text-center text-lg-start">
     <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
         © 2023 Copyright
-        <a class="text-light text-decoration-none mx-2" id="logo" href="<?=$base_url?>"><i class="fa-solid fa-book-bookmark"></i> Bookmarks&trade;Ltd</a>
+        <a class="text-light text-decoration-none mx-2 shiny-item" id="logo" href="<?=$base_url?>"><i class="fa-solid fa-book-bookmark"></i> Bookmarks&trade;Ltd</a>
     </div>
     <!-- Copyright -->
 </footer>
+<script src="assets/js/nav.js"></script>
