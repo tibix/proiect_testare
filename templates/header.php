4c11106936d6339b81b713dbe9864578578d7c41
Camelia Rasiga <rasigacamelia@gmail.com>
2023-11-28 01:39:14 +0200

    ...updating

--- templates/header.php
+++ templates/header.php
@@ -11,10 +11,7 @@
 	<title>Bookmarks</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 	<link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&display=swap" rel="stylesheet">
-<<<<<<< HEAD
-=======
 	<link rel="stylesheet" href="assets/styles/nav.css">
->>>>>>> navbar_refactoring
 	<link rel="stylesheet" href="assets/styles/main.css">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 	<script src="https://kit.fontawesome.com/6f78ace1ca.js" crossorigin="anonymous"></script>
